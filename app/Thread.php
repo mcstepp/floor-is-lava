ef4db0adbc14a63e2ccc4eeee12cedb0d73ccaa4
Meg Stepp <mcstepp@users.noreply.github.com>
2017-11-19 20:29:19 -0500

    activity feed wip

--- app/Thread.php
+++ app/Thread.php
@@ -27,6 +27,25 @@ class Thread extends Model
         static::deleting(function ($thread){
            $thread->replies()->delete();
         });
+
+        static::created(function($thread){
+            $thread->recordActivity('created');
+        });
+    }
+
+    protected function recordActivity($event)
+    {
+        Activity::create([
+            'type' => $this->getActivityType($event),
+            'user_id' => auth()->id(),
+            'subject_id' => $this->id,
+            'subject_type' => get_class($this)
+        ]);
+    }
+
+    protected function getActivityType($event)
+    {
+        return $event . '_' . strtolower((new \ReflectionClass($this))->getShortName());
     }
 
     /**
