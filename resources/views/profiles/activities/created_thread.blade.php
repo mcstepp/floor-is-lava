15bb4e2e49a6a1e2d1a62b94ab3fc4db46c5bf6e
Meg Stepp <mcstepp@users.noreply.github.com>
2017-12-02 20:12:15 -0500

    componentize activity log

--- resources/views/profiles/activities/created_thread.blade.php
+++ resources/views/profiles/activities/created_thread.blade.php
@@ -1,15 +1,12 @@
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <div class="level">
-            <span class="flex">
-                {{ $profileUser->name }} created
-                <a href="{{ $activity->subject->path() }}">
-                    {{ $activity->subject->title }}
-                </a>
-            </span>
-        </div>
-    </div>
-    <div class="panel-body">
+@component('profiles.activities.activity')
+    @slot('heading')
+        {{ $profileUser->name }} created
+        <a href="{{ $activity->subject->path() }}">
+            {{ $activity->subject->title }}
+        </a>
+    @endslot
+
+    @slot('body')
         {{ $activity->subject->body }}
-    </div>
-</div>
+    @endslot
+@endcomponent
